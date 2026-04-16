6c93adbd9b291b1b5fe7b3839e8929fb2d221194
Tsuki <feliperafaelsouza2009@gmail.com>
2026-04-16 10:52:16 -0300

    goatdot

---
+++ form_sem_design/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Poo.Apresentacao;
+
+namespace form_sem_design
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Poo.Apresentacao());
+        }
+    }
+}
---
+++ form_sem_design/apresentacao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Poo
+{
+    internal class Apresentacao : Form
+    {
+        Button btobj01 = new Button();
+        public Apresentacao()
+        {
+            this.Text = "Titulo";
+            this.Size = new Size(600, 600);
+            btobj01.Location = new Point(50, 50);
+            btobj01.Text = "Clica, imundo";
+            btobj01.Click += btobj01Click; 
+            this.Controls.Add(btobj01);
+
+        }
+        // método de evento bigga
+        private void btobj01Click(object sender , EventArgs e) 
+        {
+            MessageBox.Show("Método de Evento");
+
+        }
+    }
+
+}
